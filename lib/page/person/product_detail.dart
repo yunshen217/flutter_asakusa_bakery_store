f93c28b7949561e4b3a160659be1a23718f555f3
1534501335 <1534501335@qq.com>
2025-08-19 20:48:20 +0800

    Revert "商品详情接口查询商品详情"
    This reverts commit 4084f361633bd34cca4206d3ca63d7f3aa21152a.

--- lib/page/person/product_detail.dart
+++ lib/page/person/product_detail.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_asakusa_bakery_store/common/custom_color.dart';
 import 'package:flutter_asakusa_bakery_store/common/custom_widget.dart';
 import 'package:flutter_asakusa_bakery_store/common/info_widget.dart';
-import 'package:flutter_asakusa_bakery_store/model/product_detail_model.dart';
-import 'package:flutter_asakusa_bakery_store/page/person/mixin/product_detail_info_mixin.dart';
+import 'package:flutter_asakusa_bakery_store/page/person/product_detail_info.dart';
+import 'package:flutter_asakusa_bakery_store/page/person/product_detail_recipe.dart';
 import 'package:flutter_asakusa_bakery_store/view/BaseScaffold.dart';
-import 'package:flutter_asakusa_bakery_store/view/persion/clear_able_text_field.dart';
 import 'package:get/get.dart';
 
 /// 商品詳細
@@ -18,37 +16,15 @@ class ProductDetail extends StatefulWidget {
 }
 
 class _ProductDetailState extends State<ProductDetail>
-    with SingleTickerProviderStateMixin, ProductDetailInfoMixin {
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
+  // isHavePurge : クリアボタンが含まれていますか？
+  final arguments = Get.arguments;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    id = arguments["id"];
     _tabController = TabController(length: 2, vsync: this);
-    topTitleController =
-        List.generate(topTitle.length, (_) => TextEditingController()).obs;
-    bottomTitleController =
-        List.generate(bottomTitle.length, (_) => TextEditingController()).obs;
-    allergyInfoIsSelected
-        .assignAll(List.generate(allergyInfo.length, (_) => false.obs));
-    gettimePeriods();
-    getProductIngredientList();
-    if (id != "") {
-      getProductDetail();
-    }
-  }
-
-  @override
-  void dispose() {
-    topTitleController.forEach((c) => c.dispose);
-    bottomTitleController.forEach((c) => c.dispose);
-    productDescriptionController.dispose();
-    rawMaterialsController.dispose();
-    productDescriptionFocusNode.dispose();
-    rawMaterialsFocusNode.dispose();
-    super.dispose();
   }
 
   @override
@@ -78,10 +54,10 @@ class _ProductDetailState extends State<ProductDetail>
                   dividerHeight: 1,
                   dividerColor: CustomColor.bg,
                   splashFactory: NoSplash.splashFactory,
-                  indicatorColor: Colors.transparent,
+                  indicatorColor: Colors.transparent, 
                   indicator: UnderlineTabIndicator(
-                    borderSide:
-                        const BorderSide(color: CustomColor.redE8, width: 2),
+                    borderSide: const BorderSide(
+                        color: CustomColor.redE8, width: 2), 
                     insets: EdgeInsets.symmetric(
                       horizontal: (Get.width - (Get.width - 30) / 2) / 2,
                     ),
@@ -100,301 +76,16 @@ class _ProductDetailState extends State<ProductDetail>
               height: double.infinity,
               child: TabBarView(
                 controller: _tabController,
-                children: [
+                children: const [
                   // 詳細
-                  productDetailInfo(),
+                  ProductDetailInfo(),
                   // レシピ
-                  productDetailRecipe(),
+                  ProductDetailRecipe(),
                 ],
               ),
             ),
-            Positioned(
-                bottom: 0,
-                child: Container(
-                  width: Get.width,
-                  padding: const EdgeInsets.fromLTRB(15, 10, 15, 30),
-                  decoration:
-                      BoxDecoration(color: CustomColor.white, boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      blurRadius: 8,
-                      spreadRadius: 0,
-                      offset: const Offset(0, 4),
-                    ),
-                  ]),
-                  child: customWidget.setCupertinoButton("保存",
-                      minimumSize: Get.width - 30,
-                      height: 30,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 12,
-                      circular: 5,
-                      textColor: CustomColor.black_3,
-                      color: CustomColor.redE8, onPressed: () {
-                    updateProduct();
-                  }),
-                ))
+            infoWidget.bottomBtn("削除", "保存",arguments["isHavePurge"], () {}, () {})
           ],
         ));
-  }
-
-  Widget textEditingList(List name, RxList<TextEditingController> controller) {
-    return Column(
-      children: List.generate(name.length, (index) {
-        return Column(
-          children: [
-            infoWidget.titleWidget(name[index], false),
-            ClearableTextField(
-                controller: controller[index],
-                hintText: name[index],
-                readOnly: false),
-          ],
-        );
-      }),
-    );
-  }
-
-  Widget productDetailInfo() {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          textEditingList(topTitle, topTitleController),
-          infoWidget.titleWidget("商品画像(2枚)", false),
-          infoWidget.selectImage(
-              localAssets: image,
-              context: context,
-              maxLength: 3,
-              fileIds: fileIdList,
-              netUrls: assetsImg),
-          infoWidget.titleWidget("商品カテゴリ", false),
-          Obx(() => infoWidget.pickerSelected(productCategorySelected.value,
-                  productCategorySelected.value == "商品カテゴリ", () {
-                customWidget.showCustomizationPicker(
-                  context,
-                  columnsData: [
-                    productCategory.map((e) => e.toString()).toList()
-                  ],
-                  initialIndex: [0],
-                  title: '商品カテゴリを選択してください',
-                  confirm: (list) => productCategorySelected.value = list[0],
-                );
-              })),
-          infoWidget.titleWidget("商品焼きたて時間带", false),
-          Obx(() => infoWidget.pickerSelected(
-                  freshlyBakedTimeZoneSelected.value,
-                  freshlyBakedTimeZoneSelected.value == '商品焼きたて時間带を選択してください',
-                  () {
-                customWidget.showCustomizationPicker(
-                  context,
-                  columnsData: [
-                    freshlyBakedTimeZone.map((e) => e.toString()).toList()
-                  ],
-                  initialIndex: [0],
-                  title: '商品焼きたて時間带を選択してください',
-                  confirm: (list) =>
-                      freshlyBakedTimeZoneSelected.value = list[0],
-                );
-              })),
-          infoWidget.titleWidget("商品説明", false),
-          customWidget.setTextField(
-              productDescriptionController, productDescriptionFocusNode,
-              hintText: '',
-              circular: 5,
-              maxLines: 100,
-              height: 100,
-              maxLength: 255,
-              top: 10,
-              left: 10,
-              right: 10,
-              borderSide:
-                  const BorderSide(color: CustomColor.blackD, width: 0.5),
-              margin: const EdgeInsets.symmetric(horizontal: 15)),
-          infoWidget.titleWidget("原材料名", false),
-          customWidget.setTextField(
-              rawMaterialsController, rawMaterialsFocusNode,
-              hintText: '',
-              circular: 5,
-              maxLines: 100,
-              height: 100,
-              maxLength: 255,
-              top: 10,
-              left: 10,
-              right: 10,
-              borderSide:
-                  const BorderSide(color: CustomColor.blackD, width: 0.5),
-              margin: const EdgeInsets.symmetric(horizontal: 15)),
-          infoWidget.titleWidget("アレルギ一情報(特定8品目)", false),
-          Obx(() => Container(
-                margin: const EdgeInsets.symmetric(horizontal: 15),
-                height: 217,
-                child: SingleChildScrollView(
-                  child: GridView.count(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    crossAxisCount: 5,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 15,
-                    childAspectRatio: 0.7,
-                    children: List.generate(allergyInfo.length, (index) {
-                      return GestureDetector(
-                        onTap: () {
-                          allergyInfoIsSelected[index].value =
-                              !allergyInfoIsSelected[index].value;
-                        },
-                        child: ConstrainedBox(
-                          constraints: const BoxConstraints(maxHeight: 80),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              customWidget.setAssetsImg(
-                                  allergyInfoIsSelected[index].value
-                                      ? allergyInfo[index]["selected"]
-                                      : allergyInfo[index]["notSelected"],
-                                  width: 59,
-                                  height: 59),
-                              customWidget.setTextOverflow(
-                                  margin: const EdgeInsets.only(
-                                    top: 1,
-                                  ),
-                                  allergyInfo[index]["name"],
-                                  color: CustomColor.black_3,
-                                  fontSize: 12)
-                            ],
-                          ),
-                        ),
-                      );
-                    }),
-                  ),
-                ),
-              )),
-          infoWidget.titleWidget("ステ一タス", false),
-          Obx(() => infoWidget.pickerSelected(
-                  statusSelected.value, statusSelected.value == "ステ一タス", () {
-                customWidget.showCustomizationPicker(
-                  context,
-                  columnsData: [status.map((e) => e.toString()).toList()],
-                  initialIndex: [0],
-                  title: '商品ステ-タスを選択してください',
-                  confirm: (list) => statusSelected.value = list[0],
-                );
-              })),
-          textEditingList(bottomTitle, bottomTitleController),
-          const SizedBox(
-            height: 80,
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget productDetailRecipe() {
-    return SingleChildScrollView(
-      child: GestureDetector(
-        onTap: () {
-          for (var data in focusNodeList) {
-            data.unfocus();
-          }
-        },
-        child: Column(
-          children: [
-            Obx(() => psIngredientsList.isEmpty
-                ? Container()
-                : Column(
-                    children: List.generate(psIngredientsList.length, (index) {
-                      if (index < controllerList.length) {
-                        final item = psIngredientsList[index]!;
-                        controllerList[index].text = item.count.toString();
-                        return Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                infoWidget.titleWidget(
-                                    item.ingredientName!, false),
-                                GestureDetector(
-                                  onTap: () {
-                                    currentlyselectTheMaterial.removeAt(index);
-                                    controllerList.removeAt(index);
-                                    focusNodeList.removeAt(index);
-                                    psIngredientsList.removeAt(index);
-                                  },
-                                  child: customWidget.setAssetsImg(
-                                      "icon_clear.png",
-                                      width: 16,
-                                      height: 16,
-                                      margin: const EdgeInsets.only(right: 15)),
-                                )
-                              ],
-                            ),
-                            customWidget.setTextField(
-                              controllerList[index],
-                              focusNodeList[index],
-                              borderSide: const BorderSide(
-                                color: CustomColor.blackD,
-                                width: 0.5,
-                              ),
-                              inputFormatters: [
-                                FilteringTextInputFormatter.digitsOnly,
-                              ],
-                              isShow: true,
-                              suffixIcon: Container(
-                                margin:
-                                    const EdgeInsets.fromLTRB(15, 15, 0, 15),
-                                child: customWidget.setText(item.unit!,
-                                    color: CustomColor.black_3,
-                                    fontSize: 12,
-                                    textAlign: TextAlign.center),
-                              ),
-                              margin:
-                                  const EdgeInsets.symmetric(horizontal: 15),
-                            )
-                          ],
-                        );
-                      } else {
-                        return Container();
-                      }
-                    }),
-                  )),
-            customWidget.setOutLinedButton("材料の追加",
-                margin: const EdgeInsets.fromLTRB(15, 0, 15, 70),
-                onPressed: () => customWidget.showCustomizationPicker(context,
-                        columnsData: [
-                          selectTheMaterial.map((e) => e.toString()).toList()
-                        ],
-                        initialIndex: [0],
-                        title: '材料を選択してください', confirm: (list) {
-                      String id = "";
-                      String minUnitId = "";
-                      String unit = "";
-                      for (var data in productIngredientListModel) {
-                        if (data.ingredientName == list[0]) {
-                          id = data.id!;
-                          minUnitId = data.minUnitId!;
-                          unit = data.unit!;
-                        }
-                      }
-                      currentlyselectTheMaterial
-                          .add({"id": id, "count": "", "minUnitId": minUnitId});
-                      psIngredientsList.add(
-                          ProductDetailModelPsIngredientsList.fromJson({
-                        "id": id,
-                        "ingredientName": list[0],
-                        "count": "",
-                        "unit": unit
-                      }));
-                      controllerList.add(TextEditingController());
-                      focusNodeList.add(FocusNode());
-                    }),
-                circular: 5,
-                linewidth: 0.5,
-                minimumSize: Size(Get.width - 15, 34),
-                isHaveLeftIcon: true,
-                imgPath: "组 239@3x.png",
-                lineColor: CustomColor.blackD,
-                fontColor: Colors.black)
-          ],
-        ),
-      ),
-    );
   }
 }
--- lib/page/person/product_management.dart
+++ lib/page/person/product_management.dart
@@ -18,8 +18,8 @@ class ProductManagement extends StatefulWidget {
   State<ProductManagement> createState() => _ProductManagementState();
 }
 
-class _ProductManagementState extends State<ProductManagement>
-    with ProductManagementMixin {
+class _ProductManagementState extends State<ProductManagement> with ProductManagementMixin{
+  
   @override
   void initState() {
     super.initState();
@@ -63,6 +63,7 @@ class _ProductManagementState extends State<ProductManagement>
         circular: 0);
   }
 
+
   Widget productCard(ItemsListModel item) {
     String imgPath = "person_product_make@3x.png";
     switch (tabIndex.value) {
@@ -77,13 +78,7 @@ class _ProductManagementState extends State<ProductManagement>
         break;
     }
     return GestureDetector(
-      onTap: () {
-        Get.toNamed('/ProductDetail',
-                arguments: {"isHavePurge": true, "id": item.id.toString()})!
-            .then((_) async {
-          await getItemsList();
-        });
-      },
+      onTap: () => Routes.goPage("ProductDetail",param: {"isHavePurge":true}),
       child: Stack(
         children: [
           customWidget.setCard(
@@ -95,15 +90,7 @@ class _ProductManagementState extends State<ProductManagement>
                 children: [
                   ClipRRect(
                     borderRadius: BorderRadius.circular(10),
-                    child: item.filePath == ""
-                        ? const SizedBox(
-                            width: 60,
-                            height: 60,
-                          )
-                        : customWidget.setNetworkImg(
-                            '${Constant.base_url}${item.filePath}',
-                            width: 60,
-                            height: 60),
+                    child:item.filePath==""?const SizedBox(width: 60,height: 60,): customWidget.setNetworkImg('${Constant.base_url}${item.filePath}', width: 60, height: 60),
                   ),
                   Container(
                     margin: const EdgeInsets.only(left: 15),
@@ -115,8 +102,7 @@ class _ProductManagementState extends State<ProductManagement>
                             fontSize: 13,
                             color: CustomColor.black_3,
                             margin: const EdgeInsets.only(right: 6)),
-                        customWidget.setRichText(
-                            "累計贩壳：", "${item.totalSaleCount}",
+                        customWidget.setRichText("累計贩壳：", "${item.totalSaleCount}",
                             fontSize: 12,
                             color: CustomColor.black_9,
                             subtitleColor: CustomColor.black_3)
@@ -156,11 +142,12 @@ class _ProductManagementState extends State<ProductManagement>
               children: [
                 tabWidget(),
                 Expanded(
-                    child: Obx(() => ListView.builder(
+                    child: Obx(()=>ListView.builder(
                         itemCount: productData.length,
-                        padding: const EdgeInsets.only(bottom: 80, top: 15),
+                        padding: const EdgeInsets.only(bottom: 80,top: 15),
                         itemBuilder: (context, index) {
-                          return productCard(productData[index]);
+                          return productCard(
+                              productData[index]);
                         })))
               ],
             ),
@@ -193,8 +180,7 @@ class _ProductManagementState extends State<ProductManagement>
                     color: CustomColor.white,
                   ),
                   child: customWidget.setOutLinedButton("追加",
-                      onPressed: () => Routes.goPage("ProductDetail",
-                          param: {"isHavePurge": false, "id": ""}),
+                  onPressed: ()=>Routes.goPage("ProductDetail",param: {"isHavePurge":false}),
                       circular: 5,
                       linewidth: 0.5,
                       minimumSize: Size(Get.width - 15, 34),
